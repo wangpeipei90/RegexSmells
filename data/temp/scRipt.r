80e92647647455063b0a2ee769d523b15f291f56
Carl Chapman <carlallenchapman@gmail.com>
2016-03-08 17:18:00 -0500

    working on complete anova project and improved illustration

--- data/temp/scRipt.r
+++ data/temp/scRipt.r
@@ -1,3 +1,4 @@
-tbl = read.table("/Users/carlchapman/git/regex_readability_study/data/Rinput/E/E17_compose.tsv",TRUE,"\t")
-results = wilcox.test(tbl[,1],tbl[,2])
-capture.output(results,file="/Users/carlchapman/git/regex_readability_study/data/Routput/E/E17_compose.Rout")
+tbl = read.table("/Users/carlchapman/git/regex_readability_study/data/original/COMPLETE_ANOVA_INPUT.csv",TRUE,",")
+test_output = aov(accuracy~string*abstract*node,data=tbl)
+results=summary(test_output)
+capture.output(results,file="/Users/carlchapman/git/regex_readability_study/data/Routput/COMPLETE_ANOVA.Rout")
